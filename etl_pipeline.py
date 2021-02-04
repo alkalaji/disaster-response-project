64268a83da7e06443fa00860e7447582e4927b9e
alkalaji <6552871+alkalaji@users.noreply.github.com>
2021-02-04 11:42:20 -0500

    Create ETL pipeline

---
+++ etl_pipeline.py
@@ -0,0 +1,84 @@
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_dataset(path, index_col):
+    df = None
+    try:
+        df = pd.read_csv(path, index_col=index_col)
+    except:
+        print('Error while reading dataset: ' + path)
+    return df
+
+
+def clean_categories(categories: pd.DataFrame):
+    # create a dataframe of the 36 individual category columns
+    categories = categories.categories.str.split(pat=';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # Removing the numerical part of the column name using str split
+    category_colnames = list(row.str.split('-').str[0])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1 for the newly created columns
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-').str[1]
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    return categories
+
+
+def create_dataframe(messages, categories):
+    # merge datasets
+    df = pd.merge(messages, categories, how='inner', left_on='id', right_on='id')
+
+    print('Removing duplicates')
+    # check number of duplicates
+    dups = df.shape[0] - df.drop_duplicates().shape[0]
+    if dups > 0:
+        # drop duplicates
+        df = df.drop_duplicates()
+        print('Removed {} duplicates'.format(dups))
+    else:
+        print('No duplicates found!')
+    return df
+
+
+def write_to_db(df: pd.DataFrame):
+    try:
+        engine = create_engine('sqlite:///disaster_response.db')
+        df.to_sql('messages_and_categories', engine, index=False)
+    except:
+        print('Failed to write to database')
+
+
+if __name__ == '__main__':
+    print('Starting ETL Pipeline')
+
+    # load messages dataset
+    print('Loading "messages.csv"')
+    messages = load_dataset('./messages.csv', 'id')
+    # load categories dataset
+    print('Loading "categories.csv"')
+    categories = load_dataset('./categories.csv', 'id')
+
+    # clean categories
+    print('Cleaning categories')
+    categories = clean_categories(categories)
+
+    # create dataset dataframe
+    print('Creating dataset dataframe')
+    df = create_dataframe(messages, categories)
+
+    # write to database
+    print('Writing to database')
+    write_to_db(df)
+
+    print('Finished ETL pipeline')
