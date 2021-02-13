791c2610248772ef1c6a54740d3d4929864b33ef
alkalaji <6552871+alkalaji@users.noreply.github.com>
2021-02-13 18:58:07 -0500

    Add more charts to the web page

--- app/run.py
+++ app/run.py
@@ -1,14 +1,16 @@
 import json
 import plotly
 import pandas as pd
-
+import plotly.express as pex
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar, Heatmap
+# from sklearn.externals import joblib # outdated
+# import joblib
+import pickle
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -28,10 +30,14 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('YourTableName', engine)
+df = pd.read_sql_table('messages_and_categories', engine)
 
 # load model
-model = joblib.load("../models/best_model.pkl")
+# I was having an issue with joblib causing exceptions, so I directly used pickle insted
+# I know this is not a best practice bcause you don't want to halt your
+# page loading to do some heavy background task. But his is just a work around for now
+# model = joblib.load("../models/best_model.pkl")
+model = pickle.load(open("../models/best_model.pkl", "rb"))
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,12 +45,21 @@ model = joblib.load("../models/best_model.pkl")
 @app.route('/index')
 def index():
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
+    # Get top 5 labels and their counts
+    labels_df = df.iloc[:, 3:]
+    top_5_labels = labels_df.sum().sort_values(ascending=False).head(5)
+    top_label_names = list(top_5_labels.index)
+    top_label_counts = list(top_5_labels.values)
+
+    # Generate correlation matrix for the top 5 features in the dataset
+    top_5_corr = df.loc[:, top_label_names].corr()
+    fig = pex.bar(top_5_corr)
+
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -61,6 +76,44 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=top_label_counts,
+                    y=top_label_names,
+                    orientation='h'
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 5 message labels in the dataset',
+                'yaxis': {
+                    'title': "Label"
+                },
+                'xaxis': {
+                    'title': "Count"
+                }
+            }
+        },
+        {
+            'data': [
+                Heatmap(
+                    x=top_label_names,
+                    y=top_label_names,
+                    z=top_5_corr
+                )
+            ],
+
+            'layout': {
+                'title': 'Correlation matrix for the top 5 labels',
+                'yaxis': {
+                    'title': "Label"
+                },
+                'xaxis': {
+                    'title': "Label"
                 }
             }
         }
